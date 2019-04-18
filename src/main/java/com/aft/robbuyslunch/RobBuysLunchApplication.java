38c4bfd63fb879a59dd0991b4e321f2e29a3c5b1
yeming <yeming@DESKTOP-95A8JPQ.localdomain>
2019-04-18 10:35:35 +0100

    init

---
+++ src/main/java/com/aft/robbuyslunch/RobBuysLunchApplication.java
@@ -0,0 +1,13 @@
+package com.aft.robbuyslunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobBuysLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobBuysLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aft/robbuyslunch/RobBuysLunchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aft.robbuyslunch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobBuysLunchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
